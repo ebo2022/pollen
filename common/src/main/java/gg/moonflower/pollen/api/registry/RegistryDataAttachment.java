7ef4105cd4ce4fe59ab82c74a7ccd0a00020ef75
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-09-24 21:21:37 -0400

    They may work on fabric but not sure

--- common/src/main/java/gg/moonflower/pollen/api/registry/RegistryDataAttachment.java
+++ common/src/main/java/gg/moonflower/pollen/api/registry/RegistryDataAttachment.java
@@ -4,16 +4,24 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.Lifecycle;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import gg.moonflower.pollen.api.event.events.registry.RegisterDataAttachmentsEvent;
+
+import java.util.function.Supplier;
 
 /**
  * Allows for dynamic JSON registration of objects to the attached {@link PollinatedRegistry}.
  * <p>Only supports custom pollen registries.
  *
- * @param registry The parent registry that dynamic objects should register to
- * @param codec    The codec to serialize and deserialize registry elements
- * @param <T>      The type of dynamic registry object
+ * @param registry             The parent registry that dynamic objects should register to
+ * @param codec                The codec to serialize and deserialize registry elements
+ * @param defaultValueSupplier The default registry value
+ * @param <T>                  The type of dynamic registry object
+ *
+ * @see RegisterDataAttachmentsEvent
+ * @author ebo2022
+ * @since 2.0.0
  */
-public record RegistryDataAttachment<T>(PollinatedRegistry<T> registry, Codec<T> codec, Lifecycle lifecycle) {
+public record RegistryDataAttachment<T>(PollinatedRegistry<T> registry, Codec<T> codec, Supplier<T> defaultValueSupplier) {
 
     /**
      * @return The {@link ResourceKey} of the attached registry
--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/RegistryAccessMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/RegistryAccessMixin.java
@@ -1,12 +1,37 @@
 package gg.moonflower.pollen.core.mixin.fabric;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.google.common.collect.ImmutableMap;
+import gg.moonflower.pollen.api.event.events.registry.RegisterDataAttachmentsEvent;
+import gg.moonflower.pollen.api.registry.RegistryDataAttachment;
+import gg.moonflower.pollen.api.util.RegistryHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(RegistryAccess.class)
-public class RegistryAccessMixin {
+public interface RegistryAccessMixin {
 
-    @Mod(method = "lambda.static.1", )
+    @Inject(method = "method_30531", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/RegistryAccess;put(Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/resources/ResourceKey;Lcom/mojang/serialization/Codec;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void attachData(CallbackInfoReturnable<ImmutableMap<ResourceKey<? extends Registry<?>>, RegistryAccess.RegistryData<?>>> ci, ImmutableMap.Builder<ResourceKey<? extends Registry<?>>, RegistryAccess.RegistryData<?>> builder) {
+        RegisterDataAttachmentsEvent.EVENT.invoker().registerAttachments(new RegisterDataAttachmentsEvent.Registry() {
+            @Override
+            public <T> void register(RegistryDataAttachment<T> attachment) {
+                RegistryHelper.injectBuiltinRegistry(attachment.vanillaRegistry());
+                BuiltinRegistriesAccessor.getLoaders().put(attachment.key().location(), () -> Holder.direct(attachment.defaultValueSupplier().get()));
+                builder.put(attachment.key(), constructRegistryData(attachment));
+            }
+        });
+    }
+
+    @Unique
+    private static <T> RegistryAccess.RegistryData<T> constructRegistryData(RegistryDataAttachment<T> registryDataAttachment) {
+        return new RegistryAccess.RegistryData<>(registryDataAttachment.key(), registryDataAttachment.codec(), null);
+    }
 }
